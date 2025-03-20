e9f2b1d069c498acfe4f141071a3a6c9a49ae453
Abel <getachea@mcmaster.ca>
2025-03-20 11:42:35 -0400

    Added course class

--- Course.cpp
+++ Course.cpp
@@ -1,1 +1,33 @@
 #include "Course.h"
+
+Course::Course(std::string code, std::string name, std::string teacher, std::string room)
+    : courseCode(code), courseName(name), teacherName(teacher), classroom(room) {}
+
+// Setters
+void Course::setCourseCode(std::string code) {
+    courseCode = code;
+}
+
+void Course::setCourseName(std::string name) {
+    courseName = name;
+}
+
+void Course::setTeacherName(std::string teacher) {
+    teacherName = teacher;
+}
+
+void Course::setClassroom(std::string room) {
+    classroom = room;
+}
+
+//getters 
+
+string Course::getCourseCode() const {
+    return courseCode; 
+}
+
+string Course::getCourseName() const {
+    return courseName; 
+}
+
+
--- Course.h
+++ Course.h
@@ -1,5 +1,35 @@
 #pragma once
-class Course
-{
+#include <string>
+
+using namespace std;
+
+class Course {
+	private:
+		string courseCode;
+		string courseName;
+		string teacherName;
+		string classroom;
+
+	public:
+		//consructors
+		Course(string code, string name, string teacher, string room);
+
+		//Setters
+		void setCourseCode(std::string code);
+		void setCourseName(std::string name);
+		void setTeacherName(std::string teacher);
+		void setClassroom(std::string room);
+
+		//getters
+		std::string getCourseCode() const;
+		std::string getCourseName() const;
+		std::string getTeacherName() const;
+		std::string getClassroom() const;
+		
+		//Calculate average grade
+		double getAverageGrade() const;
+
+
 };
+
 
