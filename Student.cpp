03d993f99eb6150baf7ac60dd9666a8bfd6a2eb4
John Abbas <johnabbas330@gmail.com>
2025-03-31 15:07:34 -0400

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -5,4 +5,14 @@ Student::Student(int n, Info i, Attendance r) {
 	numCourses = n;
 	studentInfo = i;
 	record = r;
+
+
+
+
+Student::Student(int m , Info i, Attendence r) : numCourses(n), studentInfo(i), record(r) {
+    courseArrayTest = new Course[numCourses]; // Dynamically allocate array
+}
+
+Student::~Student(){
+	delete[] courseArrayTest;
 }
